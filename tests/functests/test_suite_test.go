53e283573b2ee519734d463f26f735af92c8254b
Javier Cano Cano <jcanocan@redhat.com>
2025-05-28 11:32:26 +0200

    feat(test): Add Windows Server 2008 preferences test
    It adds functional test and container disk flag for Windows Server 2008
    preferences.
    
    Signed-off-by: Javier Cano Cano <jcanocan@redhat.com>

--- tests/functests/test_suite_test.go
+++ tests/functests/test_suite_test.go
@@ -45,6 +45,8 @@ const (
 	defaultWindows10ContainerDisk          = "registry:5000/windows10-container-disk:latest"
 	defaultWindows11ContainerDisk          = "registry:5000/windows11-container-disk:latest"
 	defaultWindows2k3ContainerDisk         = "registry:5000/windows2k3-container-disk:latest"
+	defaultWindows2k8ContainerDisk         = "registry:5000/windows2k8-container-disk:latest"
+	defaultWindows2k8r2ContainerDisk       = "registry:5000/windows2k8r2-container-disk:latest"
 	defaultWindows2k16ContainerDisk        = "registry:5000/windows2k16-container-disk:latest"
 	defaultWindows2k19ContainerDisk        = "registry:5000/windows2k19-container-disk:latest"
 	defaultWindows2k22ContainerDisk        = "registry:5000/windows2k22-container-disk:latest"
@@ -73,6 +75,8 @@ var (
 	windows10ContainerDisk          string
 	windows11ContainerDisk          string
 	windows2k3ContainerDisk         string
+	windows2k8ContainerDisk         string
+	windows2k8r2ContainerDisk       string
 	windows2k16ContainerDisk        string
 	windows2k19ContainerDisk        string
 	windows2k22ContainerDisk        string
@@ -138,6 +142,10 @@ func init() {
 		defaultWindows11ContainerDisk, "Windows 11 container disk used by functional tests")
 	flag.StringVar(&windows2k3ContainerDisk, "windows-2k3-container-disk",
 		defaultWindows2k3ContainerDisk, "Windows Server 2003 container disk used by functional tests")
+	flag.StringVar(&windows2k8ContainerDisk, "windows-2k8-container-disk",
+		defaultWindows2k8ContainerDisk, "Windows Server 2008 container disk used by functional tests")
+	flag.StringVar(&windows2k8r2ContainerDisk, "windows-2k8r2-container-disk",
+		defaultWindows2k8r2ContainerDisk, "Windows Server 2008 R2 container disk used by functional tests")
 	flag.StringVar(&windows2k16ContainerDisk, "windows-2k16-container-disk",
 		defaultWindows2k16ContainerDisk, "Windows Server 2016 container disk used by functional tests")
 	flag.StringVar(&windows2k19ContainerDisk, "windows-2k19-container-disk",
